9c1233d246f8338248e367b9bcc9c8b31e47fa50
petevs <pete@petevs.com>
2023-12-13 10:19:15 -0500

    added placeholder form

--- components/EntryForm.jsx
+++ components/EntryForm.jsx
@@ -1,7 +1,66 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
 export default function EntryForm() {
-    return (
-        <div>
-            I will be the entry form
-        </div>
-    )
+  
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          username: "",
+        },
+      })
+
+      function onSubmit(values) {
+        console.log(values)
+      }
+
+  return (
+    <Form {...form}>
+      <form className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input 
+                    placeholder="shadcn" 
+                    {...field}
+                    autoComplete='off' 
+                />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
 }
--- components/NarrowContainer.jsx
+++ components/NarrowContainer.jsx
@@ -2,7 +2,7 @@ import { cn } from '@/lib/utils'
 
 export default function NarrowContainer({className, innerClassName, children}) {
     return (
-      <div className={cn("mx-auto max-w-7xl px-4 sm:px-6 lg:px-8", className)}>
+      <div className={cn("mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-10", className)}>
         <div className={cn("mx-auto max-w-7xl", innerClassName)}>{children}</div>
       </div>
     )
